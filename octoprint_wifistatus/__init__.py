23bbd02b9ec8217c88f7ea29c44a83794b71cd2f
Manuel McLure <manuel@mclure.org>
2020-10-05 11:32:12 -0700

    Changes to make plugin start working

--- octoprint_wifistatus/__init__.py
+++ octoprint_wifistatus/__init__.py
@@ -9,21 +9,24 @@ from octoprint.util import RepeatedTimer
 import sys
 import os
 
-class WifiStatusPlugin(octoprint.plugin.StartupPlugin,
+class WiFiStatusPlugin(octoprint.plugin.StartupPlugin,
 		octoprint.plugin.TemplatePlugin,
 		octoprint.plugin.AssetPlugin):
 
+    def on_after_startup(self):
+        self._logger.info("WiFiStatus loaded!")
 
 __plugin_name__ = "WiFi Status Plugin"
 __plugin_author__ = "Manuel McLure"
 __plugin_url__ = "https://github.com/ManuelMcLure/OctoPrint-WiFiStatus"
+__plugin_pythoncompat__ = ">=3.7,<4"
 
 def __plugin_load__():
     global __plugin_implementation__
     __plugin_implementation__ = WiFiStatusPlugin()
 
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config":
-            __plugin_implementation__.get_update_information
-    }
+#    global __plugin_hooks__
+#    __plugin_hooks__ = {
+#        "octoprint.plugin.softwareupdate.check_config":
+#            __plugin_implementation__.get_update_information
+#    }
