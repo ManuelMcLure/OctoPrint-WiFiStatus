a0b24c51a5e584b99a6b327e8ed9d8a9663769a4
ManuelMcLure <manuel@mclure.org>
2021-03-24 09:30:30 -0700

    Bump release number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 plugin_identifier = "wifistatus"
 plugin_package = "octoprint_{}".format(plugin_identifier)
 plugin_name = "OctoPrint_WiFiStatus"
-plugin_version = "1.5.4"
+plugin_version = "1.5.5"
 plugin_description = "Displays WiFi status on the navbar"
 plugin_author = "Manuel McLure"
 plugin_author_email = "manuel@mclure.org"
